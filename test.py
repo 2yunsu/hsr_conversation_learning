cebabbf7f85e94a1e404eff803fdccfab9b32efb
yooyoungjae <dudwo235@naver.com>
2022-03-15 14:24:40 +0900

    add speech recognition code

--- test.py
+++ test.py
@@ -4,25 +4,41 @@ import sys
 import time
 
 
+def asr_callback(msg):
+    # Threshold
+    print(msg[0], ' is recognized. ', msg[1])
+    if msg[1] > 0.5:
+        print(msg[0], msg[1], " is returned")
+        _type = 'speech'
+        _word = msg[0]
+
 def main(session):
     """
     This example uses the ALSpeechRecognition module.
     """
-    # Get the service ALSpeechRecognition.
+    audio_device = session.service("ALAudioDevice")
+    while True:
+        audio_device.setOutputVolume(3)
+        # Get the service ALSpeechRecognition.
 
-    asr_service = session.service("ALSpeechRecognition")
+        asr_service = session.service("ALSpeechRecognition")
+        memory = session.service("ALMemory")
+        asr_service.pause(True)
+        asr_service.setLanguage("English")
 
-    asr_service.setLanguage("English")
+        # Example: Adds "yes", "no" and "please" to the vocabulary (without wordspotting)
+        vocabulary = ["yes", "no", "please", 'bye']
+        asr_service.setVocabulary(vocabulary, False)
 
-    # Example: Adds "yes", "no" and "please" to the vocabulary (without wordspotting)
-    vocabulary = ["yes", "no", "please"]
-    asr_service.setVocabulary(vocabulary, False)
+        # Start the speech recognition engine with user Test_ASR
+        asr_service.subscribe("asr")
+        print 'Speech recognition engine started'
 
-    # Start the speech recognition engine with user Test_ASR
-    asr_service.subscribe("Test_ASR")
-    print 'Speech recognition engine started'
-    time.sleep(5)
-    asr_service.unsubscribe("Test_ASR")
+        asr_mem_sub = memory.subscriber("WordRecognized")
+        asr_mem_sub.signal.connect(asr_callback)
+
+        asr_service.unsubscribe("asr")
+        audio_device.setOutputVolume(100)
 
 
 if __name__ == "__main__":
