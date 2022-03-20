b2aa2a4c92a8f33c3e739fb6b0164ce0fee5b1df
2yunsu <2yunsu@snu.ac.kr>
2022-03-20 16:12:46 +0900

    2022.03.20 기본 csv 기반 cnn 코드 짬

--- cnn_csv_version.py
+++ cnn_csv_version.py
@@ -1,82 +1,36 @@
 # -*- coding: utf8 -*-
 import torch
 import torchvision
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Dataset
 import torchvision.transforms as tr
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import pandas as pd
+import os
+from sklearn.model_selection import train_test_split
+from tqdm import tqdm
+from PIL import Image
 
 
 
-"""
 class CustomImageDataset(Dataset):
-    def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
+    def __init__(self, annotations_file, img_dir, transform=None):
         self.img_labels = pd.read_csv(annotations_file, names=['.', 'label'])
-        self.img_dir = "./images"
-        self.transform = tr.Compose([tr.Resize(64), tr.ToTensor(), tr.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5))])
-        self.target_transform = target_transform
+        self.img_dir = img_dir
+        self.transform = transform
 
     def __len__(self):
         return len(self.img_labels)
 
     def __getitem__(self, idx):
         img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
-        image = read_image(img_path)
+        image = Image.open(img_path).convert('RGB')
         label = self.img_labels.iloc[idx, 1]
         if self.transform:
             image = self.transform(image)
-        if self.target_transform:
-            label = self.target_transform(label)
         return image, label
-"""
 
-"""
-class MyDataset(Dataset):
-	def __init__(self, x_data, y_data, transform=None):
-		# 텐서 변환 안함
-		self.x_data = x_data
-		self.y_data = y_data
-		self.transform = transform
-		self.len = len(y_data)
-
-	def __getitem__(self, index):
-		# 튜플 형태로 내보내기 전에 전처리 작업 실행
-		sample = self.x_data[index], self.y_data[index]
-		if self.transform:
-			sample = self.trasform(sample)
-		return sample  # 넘파이로 출력됨
-
-	def __len__(self):
-		return self.len
-
-class ToTensor:
-	def __call__(self, sample):
-		inputs, labels = sample
-		inputs = torch.FloatTensor(inputs)
-		inputs = inputs.permute(2, 0, 1)
-		return inputs, torch.LongTensor(labels)
-
-		transf = tr.Compose
-# 들어온 데이터를 연산
-class LinearTensor:
-	def __init__(self, slope=1, bias=0):
-		self.slope = slope
-		self.bias = bias
-
-	def __call__(self, sample):
-		inputs, labels = sample
-		inputs = self.slope * inputs + self.bias
-		return inputs, labels
-
-trans = tr.Compose([ToTensor(),LinearTensor(2,5)])
-ds1 = MyDataset(train_images, train_labels, transform=trans)
-train_loader1 = DataLoader(ds1, batch_size=10, shuffle=True)
-first_data = ds1[0]
-features, labels = first_data
-print(type(features), type(labels))
-"""
 
 # nn.Module을 상속 받는다.
 
@@ -105,9 +59,11 @@ class Net(nn.Module):
 if __name__ == '__main__':
 	#load_data
 	transform = tr.Compose([tr.Resize([64,64]), tr.ToTensor(), tr.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5))])
-	trainset = torchvision.datasets.ImageFolder(root="./images_class", transform=transform)
+	customset = CustomImageDataset(annotations_file='labels.csv',
+								  img_dir='images',
+								  transform=transform)
+	trainset, testset = train_test_split(customset, test_size=0.2)
 	trainloader = DataLoader(trainset, batch_size=1, shuffle=False, num_workers=2)
-	testset = torchvision.datasets.ImageFolder(root="./images_class", transform=transform)
 	testloader = DataLoader(testset, batch_size=1, shuffle=False, num_workers=2)
 
 	# 클래스에 대한 인스턴스 생성
@@ -118,7 +74,7 @@ if __name__ == '__main__':
 	criterion = nn.CrossEntropyLoss()
 	optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-	for epoch in range(5):
+	for epoch in tqdm(range(5)):
 		running_loss = 0.0
 		# traindata 불러오기(배치 형태로 들어옴)
 		for i, data in enumerate(trainloader, 0):
